e1bf47740478791786b00757db59fe399a5dc741
Rakesh <rakeshrkz2101@gmail.com>
2025-07-14 23:29:55 +0530

    Update ec2.tf

--- aws-network-setup/ec2.tf
+++ aws-network-setup/ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "web_ec2" {
-  ami           = "ami-05f991c49d264708f"  #Amazon Ubuntu 22.04 eu-west-2 
+  ami           = "ami-05f991c49d264708f"  #Amazon Ubuntu 24.04 eu-west-2 
   instance_type = "t3.micro"
   subnet_id     = aws_subnet.public.id
   associate_public_ip_address = true
