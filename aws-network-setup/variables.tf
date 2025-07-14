035192bef1c76851b2b0d901a22e1ef60b930fc8
Rakesh <58502716+rakesh-rkz@users.noreply.github.com>
2025-07-14 22:30:12 +0530

    Create variables.tf

---
+++ aws-network-setup/variables.tf
@@ -0,0 +1,19 @@
+variable "region" {
+  default = "us-west-2"
+}
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+  default = "10.0.1.0/24"
+}
+
+variable "private_subnet1_cidr" {
+  default = "10.0.2.0/24"
+}
+
+variable "private_subnet2_cidr" {
+  default = "10.0.3.0/24"
+}
