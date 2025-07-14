7e8fe3c3ac9ae842925be91d7df031221bef1c9d
Rakesh <rakeshrkz2101@gmail.com>
2025-07-14 23:29:41 +0530

    Update ec2.tf

--- aws-network-setup/ec2.tf
+++ aws-network-setup/ec2.tf
@@ -1,9 +1,15 @@
 resource "aws_instance" "web_ec2" {
-  ami           = "ami-08f79bee58074adeb"  #Amazon Ubuntu 22.04 eu-west-2
+  ami           = "ami-05f991c49d264708f"  #Amazon Ubuntu 22.04 eu-west-2 
   instance_type = "t3.micro"
   subnet_id     = aws_subnet.public.id
   associate_public_ip_address = true
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+  
+  root_block_device {
+    volume_size = 20         # Size in GB
+    volume_type = "gp3"      # General Purpose SSD (default: gp2)
+    delete_on_termination = true
+  }
 
   tags = {
     Name = "log-a-rythm-instance"
