49a3cd630fab1c6640933078b9a3c417a347028e
Rakesh <rakeshrkz2101@gmail.com>
2025-07-14 23:15:05 +0530

    rds removed & ec2 instance type modified

--- aws-network-setup/ec2.tf
+++ aws-network-setup/ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "web_ec2" {
   ami           = "ami-08f79bee58074adeb"  #Amazon Ubuntu 22.04 eu-west-2
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
   subnet_id     = aws_subnet.public.id
   associate_public_ip_address = true
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
